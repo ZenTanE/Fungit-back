6c8ada54dad70eb042293b1cdfb2279a83c4b7c2
Sara Beldjella <sarabeldjella98@gmail.com>
2025-12-04 19:22:53 +0100

    feat: create classes

---
+++ entities/search.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, idUser, idMushroom):
+        self.SetIdUser(idUser)
+        self.SetIdMushroom(idMushroom)
+    def GetIdUser(self):
+        return self.__idUser
+    def SetIdUser(self, idUser):
+        self.__idUser = idUser
+    def GetIdMushroom(self):
+        return self.__idMushroom
+    def SetIdMushroom(self, idMushroom):
+        self.__idMushroom = idMushroom
