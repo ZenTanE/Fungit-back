20163d9c19659af9a15dfcbac00da7174cd985d0
Carlos Valero <carlosvalerosala2020@gmail.com>
2025-12-04 18:37:37 +0100

    feat: PH methods

--- Api.py
+++ Api.py
@@ -1,3 +1,19 @@
 from flask import Flask, jsonify, request as req
 
 app = Flask(__name__)
+
+@app.route("/chatbot-messages/", methods = ["GET"])
+def getChatbotMessages():
+    pass
+
+@app.route("/identify-mushroom/", methods = ["POST"])
+def identifyMushroom():
+    pass
+
+@app.route("/ask-chatbot/", methods=["POST"])
+def askChatbot():
+    pass
+
+@app.route("/train-model/", methods=["PATCH"])
+def trainModel():
+    pass
