6c7b78deb0eac15da3e542d5471b0eea290de8b3
Sara Beldjella <sarabeldjella98@gmail.com>
2025-12-05 16:50:19 +0100

    refactor: select default value + add removeUser function

--- UserDAO.py
+++ UserDAO.py
@@ -6,7 +6,7 @@ class UserDAO:
     #     db.connection = db.connection
         
     @staticmethod
-    def insert(user):
+    def addUser(user):
         mycursor = db.connection.cursor()
         sql = "INSERT INTO users (name, surname, email, password) VALUES (%s, %s, %s, %s)"
         values = (user.GetName(),user.GetSurname(),user.GetEmail(),user.GetPassword(),)
@@ -15,12 +15,30 @@ class UserDAO:
         return (f"Se han añadido {mycursor.rowcount} valores")
     
     @staticmethod
-    def select():
+    def readUser(email=None):
         mycursor = db.connection.cursor()
-        mycursor.execute("SELECT id_user, name, surname, email FROM users")
-        myresult = mycursor.fetchall()
-        users = []
-        for row in myresult:
-            user = User(row[1], row[2], row[3], row[0])
-            users.append(user)
+        if email is None:
+            mycursor.execute("SELECT id_user, name, surname, email FROM users")
+            myresult = mycursor.fetchall()
+            users = []
+            for row in myresult:
+                user = User(row[1], row[2], row[3], row[0])
+                users.append(user)
+        else:
+            mycursor.execute(("SELECT id_user, name, surname, email FROM users WHERE email LIKE %s"), (email,))
+            myresult = mycursor.fetchone()
+            users = []
+            for row in myresult:
+                user = User(row[1], row[2], row[3], row[0])
+                users.append(user)
         return users
+    
+    @staticmethod
+    def removeUser(email):
+        mycursor = db.connection.cursor()
+        sql = "DELETE FROM users WHERE email = %s"
+        values = (email,)
+        mycursor.execute(sql, values)
+        db.connection.commit()
+        return (f"Se han eliminado {mycursor.rowcount} valores")
+        
