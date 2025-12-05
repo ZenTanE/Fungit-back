8fdec38bd1f3aa1f34af48a3de4de761dc037a8c
Sara Beldjella <sarabeldjella98@gmail.com>
2025-12-05 17:07:58 +0100

    refactor: add mushroom functions default value

--- MushroomDAO.py
+++ MushroomDAO.py
@@ -6,7 +6,7 @@ class MushroomDAO:
     #     db.connection = db.connection
         
     @staticmethod
-    def insert(mushroom):
+    def addMushroom(mushroom):
         mycursor = db.connection.cursor()
         sql = "INSERT INTO mushrooms (name, poisonous) VALUES (%s, %s)"
         values = (mushroom.GetName(),mushroom.GetPoisonous(),)
@@ -15,32 +15,38 @@ class MushroomDAO:
         return (f"Se han añadido {mycursor.rowcount} valores")
     
     @staticmethod
-    def select():
-        mycursor = db.connection.cursor()
-        mycursor.execute("SELECT id_mushroom, name, poisonous FROM mushrooms")
-        myresult = mycursor.fetchall()
-        mushrooms = []
-        for row in myresult:
-            mushroom = mushroom(row[1], row[2], row[3], row[0])
-            mushrooms.append(mushroom)
-        return mushrooms
-    
-    @staticmethod
-    def selectByName(name):
-        mycursor = db.connection.cursor()
-        mycursor.execute(("SELECT id_mushroom, name, poisonous FROM mushroom WHERE name LIKE %s"), (name,))
-        row = mycursor.fetchone()
-    
-        if row:
-            mushroom = Mushroom(name=row[1], poisonous=row[2], id_mushroom=row[0])
-            return mushroom
+    def readMushroom(name=None):
+        if name is None:
+            mycursor = db.connection.cursor()
+            mycursor.execute("SELECT id_mushroom, name, poisonous FROM mushrooms")
+            myresult = mycursor.fetchall()
+            mushrooms = []
+            for row in myresult:
+                mushroom = mushroom(row[1], row[2], row[3], row[0])
+                mushrooms.append(mushroom)
         else:
-            return None
+            mycursor = db.connection.cursor()
+            mycursor.execute(("SELECT id_mushroom, name, poisonous FROM mushrooms WHERE name LIKE %s"), (name,))
+            row = mycursor.fetchone()
+            mushrooms = []
+            for row in myresult:
+                mushroom = mushroom(row[1], row[2], row[3], row[0])
+                mushrooms.append(mushroom)
+        return mushrooms
+        
+    @staticmethod
+    def removeMushroom(name):
+        mycursor = db.connection.cursor()
+        sql = "DELETE FROM mushrooms WHERE name = %s"
+        values = (name,)
+        mycursor.execute(sql, values)
+        db.connection.commit()
+        return (f"Se han eliminado {mycursor.rowcount} valores")
         
     @staticmethod
     def selectPoisonous(name):
         mycursor = db.connection.cursor()
-        mycursor.execute(("SELECT id_mushroom, name, poisonous FROM mushroom WHERE name LIKE %s"), (name,))
+        mycursor.execute(("SELECT id_mushroom, name, poisonous FROM mushrooms WHERE name LIKE %s"), (name,))
         row = mycursor.fetchone()
     
         if row:
