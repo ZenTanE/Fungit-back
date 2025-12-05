b12fc18b497df7b6b5cae71d3420f065ca1fa730
Carlos Valero <carlosvalerosala2020@gmail.com>
2025-12-05 16:31:36 +0100

    feat: imageDAO select method

--- ImageDAO.py
+++ ImageDAO.py
@@ -13,5 +13,22 @@ class ImageDAO:
         return (f"Se han añadido {mycursor.rowcount} valores")
 
     @staticmethod
-    def select(images=[]):
-        pass
+    def select(image=None):
+        mycursor = db.connection.cursor()
+        if image is None:
+            mycursor.execute("SELECT image_link, id_user, id_image FROM images")
+            myresult = mycursor.fetchall()
+            images = []
+            for row in myresult:
+                image = Image(row[1], row[2], row[0])
+                images.append(image)
+        else:
+            sql = ("SELECT image_link, id_user, id_image FROM images WHERE image_link LIKE %s")
+            values = (image.GetImageLink(),)
+            mycursor.execute(sql, values)
+            myresult = mycursor.fetchall()
+            images = []
+            for row in myresult:
+                image = Image(row[1], row[2], row[0])
+                images.append(image)
+        return images
